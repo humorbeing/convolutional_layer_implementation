0005b5c2d70b60383d4f07c5e7257db2fa678b55
ray <geemguang@gmail.com>
2018-01-26 14:02:20 +0900

    from a, nothing

--- playground/sendbox.py
+++ playground/sendbox.py
@@ -1,15 +1,32 @@
 import numpy as np
 
-a = [i for i in range(9)]
+# a = [i for i in range(9)]
+# a = np.array(a)
+# print(a)
+# a = a.reshape((1, 9))
+# print(a)
+# a = a.reshape((3, 3))
+# print(a)
+# a = a.reshape((3, 1, 3))
+# print(a)
+# a = a.reshape((9, 1))
+# print(a)
+# a = a.reshape((3, 3))
+# print(a)
+
+a = [i for i in range(24)]
+b = [i for i in range(12)]
+
 a = np.array(a)
+b = np.array(b)
+a = a.reshape((2, 3, 4))
+b = b.reshape((4, 3))
+
+
+print(a.shape)
+print(b.shape)
+c = np.matmul(a, b)
+print(c.shape)
 print(a)
-a = a.reshape((1, 9))
-print(a)
-a = a.reshape((3, 3))
-print(a)
-a = a.reshape((3, 1, 3))
-print(a)
-a = a.reshape((9, 1))
-print(a)
-a = a.reshape((3, 3))
-print(a)
+print(b)
+print(c)
